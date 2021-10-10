250c4824adbda131ca4d6d161e7389408410c0bf
flacial <ruthemiumwithdiamond@protonmail.com>
2021-10-10 14:24:20 -0400

    Fix gh-pages script

--- gh-pages.js
+++ gh-pages.js
@@ -1,16 +1,3 @@
 import ghpages from 'gh-pages';
 
-ghpages.publish(
-    'public', // path to public directory
-    {
-        branch: 'gh-pages',
-        repo: 'https://github.com/flacial/image-uploader', // Update to point to your repository  
-        user: {
-            name: 'flacial', // update to use your name
-            email: 'ruthemiumwithdiamond@protonmail.com' // Update to use your email
-        }
-    },
-    () => {
-        console.log('Deploy Complete!')
-    }
-)
+ghpages.publish('dist')
