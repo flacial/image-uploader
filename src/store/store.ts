b1704e4930603f5dbc67040fa1791934c97b74c8
flacial <ruthemiumwithdiamond@protonmail.com>
2021-10-10 13:57:23 -0400

    Updates

--- src/store/store.ts
+++
@@ -1,6 +0,0 @@
-import { writable } from "svelte/store";
-
-export const imageBuffer = writable<string>("");
-export const imageUrl = writable("");
-export const imageName = writable<string>("");
-export const isError = writable<boolean>(false);
