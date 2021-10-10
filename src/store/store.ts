ec63be51ab8fd8bae27bfa955a96e0020f6f5467
flacial <ruthemiumwithdiamond@protonmail.com>
2021-10-10 13:50:36 -0400

    store: Change state type

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import { writable } from "svelte/store";
 
-export const imageBuffer = writable<string | ArrayBuffer>("");
-export const imageUrl = writable<string>("");
+export const imageBuffer = writable<string>("");
+export const imageUrl = writable("");
 export const imageName = writable<string>("");
 export const isError = writable<boolean>(false);
